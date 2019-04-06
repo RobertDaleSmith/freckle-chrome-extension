bda19ade7c748976611daef8b763ba67a6186dd3
Robert Dale Smith <robert@robertdalesmith.com>
2019-04-06 14:01:15 -0500

    refactor af, es6, style guide

--- popup.js
+++ popup.js
@@ -1,160 +1,189 @@
-let addNewShowing = false;
-let editAccountName = "";
-let accountList = [];
+let addedAccounts = [];
 
-function onLoad() {
-  const orgName = localStorage['organization-name'];
-  if (orgName) {
-    connectToAccount(orgName);
-  } else {
-    document.querySelector('#container').className = "error";
-  }
+let editShowing = null;
 
-  if (localStorage['organization-list']) {
-    accountList = JSON.parse(localStorage['organization-list']);
-    for (let i=0; i < accountList.length; i++) {
-      insertSavedAccountToList(accountList[i].name, accountList[i].color);
-    }
-  }
-  if (accountList.length < 1) showAddNewAccountEvent({});
+let editingId = null;
 
-  document.querySelector('#add_new_account').onclick = showAddNewAccountEvent;
-  document.querySelector('#cancel_new_project').onclick = hideAddNewAccountEvent;
-  document.querySelector('#curtain').onclick = hideAddNewAccountEvent;
-  document.querySelector('#save_new_project').onclick = addNewAccountEvent;
-  document.querySelector('#show_accounts_btn').onclick = disconnectAccount;
+const accountList = document.querySelector('#account-list');
 
-  document.querySelector('#remove_account').onclick = (event) => {
-    removeAccount(editAccountName);
-    hideAddNewAccountEvent();
-  };
+const addButton = document.querySelector('#add-button');
 
-  document.querySelector('#new_project_name').onkeypress = (event) => {
-    if (event.keyCode === 13) {
-      addNewAccountEvent();
-    } else if (event.keyCode === 27) {
-      hideAddNewAccountEvent();
-    }
-  };
-}
-window.onload = onLoad();
+const backButton = document.querySelector('#back-button');
+
+const bodyWrapper = document.querySelector('#body-wrapper');
+
+const editMenu = document.querySelector('#edit-menu');
+
+const editMenuCancelButton = document.querySelector('#edit-menu-cancel-button');
+
+const editMenuDiscardOption = document.querySelector('#edit-menu-discard-option');
+
+const editMenuDiscardButton = document.querySelector('#edit-menu-discard-button');
+
+const editMenuInput = document.querySelector('#edit-menu-input');
+
+const editMenuLabel = document.querySelector('#edit-menu-label');
+
+const editMenuSaveButton = document.querySelector('#edit-menu-save-button');
+
+const overlayMask = document.querySelector('#overlay-mask');
+
+const timerFrame = document.querySelector('#timer-frame');
+
+const colors = ['#ef9655','#55c9ef','#ef5555','#f1f353','#bce162','#13a480','#ef63a2'];
 
 const getRandomInt = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
 
-function showAddNewAccountEvent({ name }) {
-  if (!addNewShowing) {
-    if (!name) {
-      document.querySelector('#create_project_title').textContent = "Add a New Account";
-    } else {
-      editAccountName = accountNameToID(name).replace("id_", "");
-      document.querySelector('#create_project_title').textContent = "Edit Account";
-      document.querySelector('#deleteExisting').style.display = "block";
-      document.querySelector('#new_project_name').value = name;
-    }
-    addNewShowing = true;
-    slideAddNewAccount(true);
-    fadeCurtain(true);
-  }
+const getRandomColor = () => colors[getRandomInt(0, 6)];
+
+const replaceRegex = `https?://|(nokotime)?(letsfreckle)?.com|[!@#$%^&*(){}\\[\\]?,./\\\:;\`\~\\'\\"\\|]`;
+
+const nameToId = (name) => {
+  const match = new RegExp(`${replaceRegex}|\\s`, 'g');
+  return `id_${name.replace(match, "").toLowerCase()}`;
 }
 
-function hideAddNewAccountEvent() {
-	if (addNewShowing) {
-    addNewShowing = false;
-    slideAddNewAccount(false);
-    fadeCurtain(false)
-    editAccountName = "";
-    document.querySelector('#deleteExisting').style.display = "none";
-  }
-}
-
-function fadeCurtain(fadeIn) {
-  let val = 0;
-  let delta = 0.1;
-  if (!fadeIn) {
-    val = 1;
-    delta = -0.1;
+function onLoad() {
+  const accountName = localStorage['account-name'];
+  if (accountName) {
+    openAccount(accountName);
   } else {
-    document.querySelector('#curtain').style.display = "block";
+    bodyWrapper.className = "show-accounts";
   }
-  const interval = setInterval(function(){
-    document.querySelector('#curtain').style.opacity = val;
-    val = val + delta;
-    if(val <= 0) document.querySelector('#curtain').style.display = "none";
-    if(val >= 1 || val <= 0) clearInterval(interval);
-  }, 1);
-}
 
-function slideAddNewAccount(slideIn) {
-  let val = -72;
-  let delta = 2;
-  if (!slideIn) {
-    val = 32;
-    delta = -2;
-  }
-  const interval = setInterval(function(){
-    document.querySelector('#create_project').style.top = `${val}px`;
-    val = val + delta;
-    if (val >= 34 || val <= -72) { 
-      if (val <= -70) document.querySelector('#new_project_name').value = "";
-      if (val >=  34) document.querySelector('#new_project_name').focus();
-      clearInterval(interval); 
+  if (localStorage['account-list']) {
+    addedAccounts = JSON.parse(localStorage['account-list']);
+    for (let i=0; i < addedAccounts.length; i++) {
+      addAccountListItem(addedAccounts[i]);
     }
-  }, 1);
-}
-
-function addNewAccountEvent() {
-  let newAccountName = document.querySelector('#new_project_name').value;
-  newAccountName = newAccountName.replace("https://", "").replace("http://", "");
-  newAccountName = newAccountName.split(".letsfreckle.com")[0];
-  newAccountName = newAccountName.split(".nokotime.com")[0];
-  if (newAccountName !== "" && (
-      GetElementInsideContainer('account-list', accountNameToID(newAccountName)) === null ||
-      accountNameToID(newAccountName) === accountNameToID(editAccountName)
-  )) {
-    if(editAccountName === ""){
-      addAccount(newAccountName);
-      hideAddNewAccountEvent();
-    } else {
-      updateAccount(editAccountName, newAccountName);
-      hideAddNewAccountEvent();
-    }
-  } else {
-    accountNameErrorFlash();
   }
+  if (addedAccounts.length < 1) showAccountEdit();
+
+  addButton.onclick = showAccountEdit;
+  backButton.onclick = showAccounts;
+  editMenuCancelButton.onclick = hideAccountEdit;
+  editMenuSaveButton.onclick = saveAccountEdit;
+  overlayMask.onclick = hideAccountEdit;
+
+  editMenuDiscardButton.onclick = (event) => {
+    removeAccount(editingId);
+    hideAccountEdit();
+  };
+
+  editMenuInput.onkeypress = (event) => {
+    if (event.keyCode === 13) saveAccountEdit();
+    else if (event.keyCode === 27) hideAccountEdit();
+  };
 }
 
-function GetElementInsideContainer(containerID, childID) {
-  const element = document.getElementById(childID);
-  const parent = element ? element.parentNode : {};
-  return (parent.id && parent.id === containerID) ? element : null;
-}
+window.onload = onLoad();
 
-function accountNameErrorFlash() {
+function animateAccountEditInputError() {
   let hexVal = 255;
   let down = true;
   let delta = 5;
 
   const interval = setInterval(function(){
-    document.querySelector('#new_project_name').style.backgroundColor = `rgb(255,${hexVal},${hexVal})`;
+    editMenuInput.style.backgroundColor = `rgb(255, ${hexVal}, ${hexVal})`;
     if (down) {
       hexVal = hexVal - delta;
       if(hexVal <= 150) down = false;
     } else {
       hexVal = hexVal + delta;
-      if(hexVal >= 255) { 
-        down = true; 
-        document.querySelector('#new_project_name').style.backgroundColor = "rgb(255,255,255)";
-        document.querySelector('#new_project_name').focus();
+      if(hexVal >= 255) {
+        down = true;
+        editMenuInput.style.backgroundColor = "rgb(255, 255, 255)";
+        editMenuInput.focus();
         clearInterval(interval);
-      }        
+      }
     }
   }, 5);
 }
 
-function insertSavedAccountToList(name, color) {
+function animateOverlayMaskVisibility(show = true) {
+  let val = 0;
+  let delta = 0.1;
+
+  if (!show) {
+    val = 1;
+    delta = -0.1;
+  } else {
+    overlayMask.style.display = "block";
+  }
+
+  const interval = setInterval(function(){
+    overlayMask.style.opacity = val;
+    val = val + delta;
+    if(val <= 0) overlayMask.style.display = "none";
+    if(val >= 1 || val <= 0) clearInterval(interval);
+  }, 5);
+}
+
+function animateAccountEditVisibility(show = true) {
+  let val = -72;
+  let delta = 2;
+  if (!show) {
+    val = 32;
+    delta = -2;
+  }
+  const interval = setInterval(function(){
+    editMenu.style.top = `${val}px`;
+    val = val + delta;
+    if (val >= 34 || val <= -72) { 
+      if (val <= -70) editMenuInput.value = "";
+      if (val >=  34) editMenuInput.focus();
+      clearInterval(interval); 
+    }
+  }, 1);
+}
+
+function hideAccountEdit() {
+  if (editShowing) {
+    editingId = null;
+    editShowing = false;
+    animateAccountEditVisibility(false);
+    animateOverlayMaskVisibility(false);
+    editMenuDiscardOption.style.display = "none";
+  }
+}
+
+function saveAccountEdit() {
+  const value = editMenuInput.value || "";
+  const match = new RegExp(replaceRegex, 'g');
+  const name = value.replace(match, "");
+  const newId = nameToId(name);
+
+  if (name && (!document.querySelector(`#${newId}`) || editingId === newId)) {
+    if(!editingId){
+      addAccount({ name });
+      hideAccountEdit();
+    } else {
+      updateAccount({ id: editingId, name });
+      hideAccountEdit();
+    }
+  } else {
+    animateAccountEditInputError();
+  }
+}
+
+function addAccount({ name }) {
+  if (!name) return;
+
+  const newAccount = { name, color: getRandomColor() };
+  addAccountListItem(newAccount);
+  addedAccounts.push(newAccount);
+
+  //Add to local storage.
+  localStorage['account-list'] = JSON.stringify(addedAccounts);
+}
+
+function addAccountListItem({ name, color }) {
+  if (!name) return;
+  if (!color) color = getRandomColor();
+
   const li = document.createElement('li');
-  li.className = "project";
-  li.id = accountNameToID(name);
+  li.className = "account";
+  li.id = nameToId(name);
 
   const div = document.createElement('div');
   div.className = "options";
@@ -164,115 +193,88 @@ function insertSavedAccountToList(name, color) {
   a.setAttribute("href", "#");
 
   a.onclick = (e) => {
-    showAddNewAccountEvent({
-      name: e.target.closest('li').getElementsByClassName('account-title')[0].textContent,
-    });
+    const name = e.target.closest('li').querySelector('.account-title').textContent || null;
+    showAccountEdit({ name });
   };
 
   div.appendChild(a);
   li.appendChild(div);
   
   const h2 = document.createElement("h2");
-  h2.onclick = (e) => connectToAccount(e.target.closest('li').id.replace("id_", ""));
+  h2.onclick = (e) => openAccount(e.target.closest('li').id.replace("id_", ""));
   h2.innerHTML = `
-    <span class="project-name">
+    <span class="account-name">
       <div class="color-box" style="background-color: ${color}"></div>
       <span class="account-title">${name}</span>
     </span>
   `;
 
   li.appendChild(h2);
-
-  document.querySelector('#account-list').appendChild(li);
+  accountList.appendChild(li);
 }
 
-function getRandomColor(name, color) {
-  const baseColors = ['#ef9655','#55c9ef','#ef5555','#f1f353','#bce162','#13a480','#ef63a2'];
-  return baseColors[getRandomInt(0, 6)];
+function openAccount(name) {
+  bodyWrapper.className = "";
+  timerFrame.setAttribute("src", `https://${name}.nokotime.com/timer`);
+  backButton.style.display = "block";
+  localStorage['account-name'] = name;
 }
 
-function addAccount(name) {
-  const randomColor = getRandomColor();
-  insertSavedAccountToList(name, randomColor);
-  accountList.push({name: name, color: randomColor});
-
-  //Add to local storage.
-  localStorage["organization-list"] = JSON.stringify(accountList);
+function showAccounts() {
+  bodyWrapper.className = "show-accounts";
+  timerFrame.setAttribute("src", "");
+  backButton.style.display = "none";
+  localStorage['account-name'] = "";
 }
 
-function removeAccount(name) {
-  console.log(name);
-  document.getElementById(accountNameToID(name)).remove();
-  for (let i = 0; i < accountList.length; i++) {
-    if (accountNameToID(accountList[i].name) === accountNameToID(name)) {
-      accountList.splice(i, 1);
+function showAccountEdit(options = {}) {
+  const { name, color } = options;
+
+  if (!editShowing) {
+    if (!name) {
+      editMenuLabel.textContent = "Add a New Account";
+    } else {
+      editingId = nameToId(name);
+      editMenuLabel.textContent = "Edit Account";
+      editMenuDiscardOption.style.display = "block";
+      editMenuInput.value = name;
+    }
+    editShowing = true;
+    animateAccountEditVisibility();
+    animateOverlayMaskVisibility();
+  }
+}
+
+function removeAccount(id) {
+  console.log(id);
+  document.querySelector(`#${id}`).remove();
+  for (let i = 0; i < addedAccounts.length; i++) {
+    if (nameToId(addedAccounts[i].name) === id) {
+      addedAccounts.splice(i, 1);
       break;
     }
   }
 
   //Remove from local storage.
-  localStorage["organization-list"] = JSON.stringify(accountList);
+  localStorage['account-list'] = JSON.stringify(addedAccounts);
 }
 
-function updateAccount(oldName, newName) {
-  const updateID = oldName.split(' ').join('').toLowerCase()
-  const liElement = document.getElementById(accountNameToID(oldName));
-  liElement.getElementsByClassName('account-title')[0].textContent = newName;
-  liElement.id = accountNameToID(newName);
+function updateAccount(options = {}) {
+  const { id, name } = options;
+  if (!id || !name) return;
 
-  for (let i = 0; i < accountList.length; i++) {
-    if (accountNameToID(accountList[i].name) === accountNameToID(oldName)) {
-      accountList[i].name = newName;
+  const liElement = document.querySelector(`#${id}`);
+  liElement.querySelector('.account-title').textContent = name;
+  liElement.id = nameToId(name);
+
+  for (let i = 0; i < addedAccounts.length; i++) {
+    if (nameToId(addedAccounts[i].name) === id) {
+      addedAccounts[i].name = name;
       break;
     }
   }
   //Update in local storage.
-  localStorage["organization-list"] = JSON.stringify(accountList);
-}
-
-function accountNameToID(name) {
-  return "id_" + name
-    .replace("https://", "")
-    .replace(".letsfreckle.com/", "")
-    .replace(".letsfreckle.com", "")
-    .replace(".nokotime.com/", "")
-    .replace(".nokotime.com", "")
-    .split('!').join('').split(',').join('')
-    .split('/').join('').split('\\').join('')
-    .split('[').join('').split(']').join('')
-    .split('{').join('').split('}').join('')
-    .split('?').join('').split('.').join('')
-    .split(',').join('').split('$').join('')
-    .split('#').join('').split('@').join('')
-    .split('%').join('').split('^').join('')
-    .split('*').join('').split(' ').join('')
-    .toLowerCase();
-}
-
-function connectToAccount(name) {
-  document.querySelector('#container').className = "";
-  document.querySelector('#timer').setAttribute("src", `https://${name}.nokotime.com/timer`);
-  document.querySelector('#show_accounts_btn').style.display = "block";
-  localStorage["organization-name"] = name;
-}
-
-function disconnectAccount() {
-  document.querySelector('#container').className = "error";
-  document.querySelector('#timer').setAttribute("src", "");
-  document.querySelector('#show_accounts_btn').style.display = "none";
-  localStorage["organization-name"] = "";
-}
-
-Element.prototype.remove = function() {
-  this.parentElement.removeChild(this);
-}
-
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-  for (let i = 0, len = this.length; i < len; i++) {
-    if (this[i] && this[i].parentElement) {
-      this[i].parentElement.removeChild(this[i]);
-    }
-  }
+  localStorage['account-list'] = JSON.stringify(addedAccounts);
 }
 
 // Add the header to allow content to be iframed
