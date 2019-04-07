323a812a63838dbad22a746e0a7ece6aa7a07ae2
Robert Dale Smith <robert@robertdalesmith.com>
2019-04-06 22:59:56 -0500

    more refactoring; back button text rather than icon

--- popup.js
+++ popup.js
@@ -1,90 +1,210 @@
-let addedAccounts = [];
-
-let editShowing = null;
-
+let accounts = [];
 let editingId = null;
+let isEditing = null;
 
 const accountList = document.querySelector('#account-list');
-
 const addButton = document.querySelector('#add-button');
-
 const backButton = document.querySelector('#back-button');
-
 const bodyWrapper = document.querySelector('#body-wrapper');
-
-const editMenu = document.querySelector('#edit-menu');
-
-const editMenuCancelButton = document.querySelector('#edit-menu-cancel-button');
-
-const editMenuDiscardOption = document.querySelector('#edit-menu-discard-option');
-
-const editMenuDiscardButton = document.querySelector('#edit-menu-discard-button');
-
-const editMenuInput = document.querySelector('#edit-menu-input');
-
-const editMenuLabel = document.querySelector('#edit-menu-label');
-
-const editMenuSaveButton = document.querySelector('#edit-menu-save-button');
-
+const cancelButton = document.querySelector('#edit-menu-cancel-button');
+const discardOption = document.querySelector('#edit-menu-discard-option');
+const discardButton = document.querySelector('#edit-menu-discard-button');
+const editBlock = document.querySelector('#edit-menu');
+const editInput = document.querySelector('#edit-menu-input');
+const editLabel = document.querySelector('#edit-menu-label');
 const overlayMask = document.querySelector('#overlay-mask');
-
+const saveButton = document.querySelector('#edit-menu-save-button');
 const timerFrame = document.querySelector('#timer-frame');
 
 const colors = ['#ef9655','#55c9ef','#ef5555','#f1f353','#bce162','#13a480','#ef63a2'];
+const cleanR = `https?://|(nokotime)?(letsfreckle)?.com|[!@#$%^&*(){}\\[\\]?,./\\\:;\`\~\\'\\"\\|]`;
+const nameToId = (name) => `id_${name.replace(new RegExp(`${cleanR}|\\s`, 'g'), "").toLowerCase()}`;
+const randomInt = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
+const randomColor = () => colors[randomInt(0, 6)];
 
-const getRandomInt = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
-
-const getRandomColor = () => colors[getRandomInt(0, 6)];
-
-const replaceRegex = `https?://|(nokotime)?(letsfreckle)?.com|[!@#$%^&*(){}\\[\\]?,./\\\:;\`\~\\'\\"\\|]`;
-
-const nameToId = (name) => {
-  const match = new RegExp(`${replaceRegex}|\\s`, 'g');
-  return `id_${name.replace(match, "").toLowerCase()}`;
-}
-
-function onLoad() {
+function init() {
   const accountName = localStorage['account-name'];
-  if (accountName) {
-    openAccount(accountName);
-  } else {
-    bodyWrapper.className = "show-accounts";
-  }
+
+  if (!accountName) onShowAccounts();
+  else onOpenAccount(accountName);
 
   if (localStorage['account-list']) {
-    addedAccounts = JSON.parse(localStorage['account-list']);
-    for (let i=0; i < addedAccounts.length; i++) {
-      addAccountListItem(addedAccounts[i]);
+    accounts = JSON.parse(localStorage['account-list']);
+    for (let i=0; i < accounts.length; i++) {
+      renderAccount(accounts[i]);
     }
   }
-  if (addedAccounts.length < 1) showAccountEdit();
 
-  addButton.onclick = showAccountEdit;
-  backButton.onclick = showAccounts;
-  editMenuCancelButton.onclick = hideAccountEdit;
-  editMenuSaveButton.onclick = saveAccountEdit;
-  overlayMask.onclick = hideAccountEdit;
+  if (accounts.length < 1) onEditAccount();
 
-  editMenuDiscardButton.onclick = (event) => {
-    removeAccount(editingId);
-    hideAccountEdit();
-  };
-
-  editMenuInput.onkeypress = (event) => {
-    if (event.keyCode === 13) saveAccountEdit();
-    else if (event.keyCode === 27) hideAccountEdit();
-  };
+  addButton.onclick = onEditAccount;
+  backButton.onclick = onShowAccounts;
+  cancelButton.onclick = onCancelEdit;
+  discardButton.onclick = onDiscardAccount;
+  editInput.onkeypress = onEditInput;
+  overlayMask.onclick = onCancelEdit;
+  saveButton.onclick = onSaveAccount;
 }
 
-window.onload = onLoad();
+window.onload = init();
 
-function animateAccountEditInputError() {
+function onAddAccount(options = {}) {
+  const { name } = options;
+  if (!name) return;
+
+  const newAccount = { name, color: randomColor() };
+  renderAccount(newAccount);
+  accounts.push(newAccount);
+
+  //Add to local storage.
+  localStorage['account-list'] = JSON.stringify(accounts);
+}
+
+function onCancelEdit() {
+  if (isEditing) {
+    editingId = null;
+    isEditing = false;
+    toggleEditAccount(false);
+    toggleOverlayMask(false);
+    discardOption.style.display = "none";
+  }
+}
+
+function onDiscardAccount() {
+  const id = editingId;
+  console.log(id);
+
+  document.querySelector(`#${id}`).remove();
+
+  for (let i = 0; i < accounts.length; i++) {
+    if (nameToId(accounts[i].name) === id) {
+      accounts.splice(i, 1);
+      break;
+    }
+  }
+
+  //Remove from local storage.
+  localStorage['account-list'] = JSON.stringify(accounts);
+
+  onCancelEdit();
+}
+
+function onEditAccount(options = {}) {
+  const { name } = options;
+
+  if (!isEditing) {
+    if (!name) {
+      editLabel.textContent = "Add a New Account";
+    } else {
+      editingId = nameToId(name);
+      editLabel.textContent = "Edit Account";
+      discardOption.style.display = "block";
+      editInput.value = name;
+    }
+    isEditing = true;
+    toggleEditAccount();
+    toggleOverlayMask();
+  }
+}
+
+function onEditInput(event) {
+  if (event.keyCode === 13) onSaveAccount();
+  else if (event.keyCode === 27) onCancelEdit();
+}
+
+function onOpenAccount(name) {
+  bodyWrapper.className = "";
+  timerFrame.setAttribute("src", `https://${name}.nokotime.com/timer`);
+  backButton.style.display = "block";
+  localStorage['account-name'] = name;
+}
+
+function onSaveAccount() {
+  const value = editInput.value || "";
+  const match = new RegExp(cleanR, 'g');
+  const name = value.replace(match, "");
+  const newId = nameToId(name);
+
+  if (name && (!document.querySelector(`#${newId}`) || editingId === newId)) {
+    if(!editingId){
+      onAddAccount({ name });
+      onCancelEdit();
+    } else {
+      onUpdateAccount({ id: editingId, name });
+      onCancelEdit();
+    }
+  } else {
+    strobeError();
+  }
+}
+
+function onShowAccounts() {
+  bodyWrapper.className = "show-accounts";
+  timerFrame.setAttribute("src", "");
+  backButton.style.display = "none";
+  localStorage['account-name'] = "";
+}
+
+function onUpdateAccount(options = {}) {
+  const { id, name } = options;
+  if (!id || !name) return;
+
+  const liElement = document.querySelector(`#${id}`);
+  liElement.querySelector('.account-title').textContent = name;
+  liElement.id = nameToId(name);
+
+  for (let i = 0; i < accounts.length; i++) {
+    if (nameToId(accounts[i].name) === id) {
+      accounts[i].name = name;
+      break;
+    }
+  }
+  //Update in local storage.
+  localStorage['account-list'] = JSON.stringify(accounts);
+}
+
+function renderAccount(options = {}) {
+  let { name, color } = options;
+
+  if (!name) return;
+  if (!color) color = randomColor();
+
+  const id = nameToId(name);
+
+  const listItem = document.createElement('li');
+  listItem.className = "account";
+  listItem.id = id;
+
+  const optionsBlock = document.createElement('div');
+  optionsBlock.className = "options";
+
+  const editButton = document.createElement('a');
+  editButton.onclick = (e) => onEditAccount({ name });
+  editButton.className = "play icon-cog2";
+  editButton.setAttribute("href", "#");
+  
+  const titleBlock = document.createElement("h2");
+  titleBlock.onclick = (e) => onOpenAccount(id.replace("id_", ""));
+  titleBlock.innerHTML = `
+    <span class="account-name">
+      <div class="color-box" style="background-color: ${color}"></div>
+      <span class="account-title">${name}</span>
+    </span>
+  `;
+
+  optionsBlock.appendChild(editButton);
+  listItem.appendChild(optionsBlock);
+  listItem.appendChild(titleBlock);
+  accountList.appendChild(listItem);
+}
+
+function strobeError() {
   let hexVal = 255;
   let down = true;
   let delta = 5;
 
   const interval = setInterval(function(){
-    editMenuInput.style.backgroundColor = `rgb(255, ${hexVal}, ${hexVal})`;
+    editInput.style.backgroundColor = `rgb(255, ${hexVal}, ${hexVal})`;
     if (down) {
       hexVal = hexVal - delta;
       if(hexVal <= 150) down = false;
@@ -92,15 +212,15 @@ function animateAccountEditInputError() {
       hexVal = hexVal + delta;
       if(hexVal >= 255) {
         down = true;
-        editMenuInput.style.backgroundColor = "rgb(255, 255, 255)";
-        editMenuInput.focus();
+        editInput.style.backgroundColor = "rgb(255, 255, 255)";
+        editInput.focus();
         clearInterval(interval);
       }
     }
   }, 5);
 }
 
-function animateOverlayMaskVisibility(show = true) {
+function toggleOverlayMask(show = true) {
   let val = 0;
   let delta = 0.1;
 
@@ -119,7 +239,7 @@ function animateOverlayMaskVisibility(show = true) {
   }, 5);
 }
 
-function animateAccountEditVisibility(show = true) {
+function toggleEditAccount(show = true) {
   let val = -72;
   let delta = 2;
   if (!show) {
@@ -127,154 +247,14 @@ function animateAccountEditVisibility(show = true) {
     delta = -2;
   }
   const interval = setInterval(function(){
-    editMenu.style.top = `${val}px`;
+    editBlock.style.top = `${val}px`;
     val = val + delta;
     if (val >= 34 || val <= -72) { 
-      if (val <= -70) editMenuInput.value = "";
-      if (val >=  34) editMenuInput.focus();
+      if (val <= -70) editInput.value = "";
+      if (val >=  34) editInput.focus();
       clearInterval(interval); 
     }
   }, 1);
-}
-
-function hideAccountEdit() {
-  if (editShowing) {
-    editingId = null;
-    editShowing = false;
-    animateAccountEditVisibility(false);
-    animateOverlayMaskVisibility(false);
-    editMenuDiscardOption.style.display = "none";
-  }
-}
-
-function saveAccountEdit() {
-  const value = editMenuInput.value || "";
-  const match = new RegExp(replaceRegex, 'g');
-  const name = value.replace(match, "");
-  const newId = nameToId(name);
-
-  if (name && (!document.querySelector(`#${newId}`) || editingId === newId)) {
-    if(!editingId){
-      addAccount({ name });
-      hideAccountEdit();
-    } else {
-      updateAccount({ id: editingId, name });
-      hideAccountEdit();
-    }
-  } else {
-    animateAccountEditInputError();
-  }
-}
-
-function addAccount({ name }) {
-  if (!name) return;
-
-  const newAccount = { name, color: getRandomColor() };
-  addAccountListItem(newAccount);
-  addedAccounts.push(newAccount);
-
-  //Add to local storage.
-  localStorage['account-list'] = JSON.stringify(addedAccounts);
-}
-
-function addAccountListItem({ name, color }) {
-  if (!name) return;
-  if (!color) color = getRandomColor();
-
-  const li = document.createElement('li');
-  li.className = "account";
-  li.id = nameToId(name);
-
-  const div = document.createElement('div');
-  div.className = "options";
-
-  const a = document.createElement('a');
-  a.className = "play icon-cog2";
-  a.setAttribute("href", "#");
-
-  a.onclick = (e) => {
-    const name = e.target.closest('li').querySelector('.account-title').textContent || null;
-    showAccountEdit({ name });
-  };
-
-  div.appendChild(a);
-  li.appendChild(div);
-  
-  const h2 = document.createElement("h2");
-  h2.onclick = (e) => openAccount(e.target.closest('li').id.replace("id_", ""));
-  h2.innerHTML = `
-    <span class="account-name">
-      <div class="color-box" style="background-color: ${color}"></div>
-      <span class="account-title">${name}</span>
-    </span>
-  `;
-
-  li.appendChild(h2);
-  accountList.appendChild(li);
-}
-
-function openAccount(name) {
-  bodyWrapper.className = "";
-  timerFrame.setAttribute("src", `https://${name}.nokotime.com/timer`);
-  backButton.style.display = "block";
-  localStorage['account-name'] = name;
-}
-
-function showAccounts() {
-  bodyWrapper.className = "show-accounts";
-  timerFrame.setAttribute("src", "");
-  backButton.style.display = "none";
-  localStorage['account-name'] = "";
-}
-
-function showAccountEdit(options = {}) {
-  const { name, color } = options;
-
-  if (!editShowing) {
-    if (!name) {
-      editMenuLabel.textContent = "Add a New Account";
-    } else {
-      editingId = nameToId(name);
-      editMenuLabel.textContent = "Edit Account";
-      editMenuDiscardOption.style.display = "block";
-      editMenuInput.value = name;
-    }
-    editShowing = true;
-    animateAccountEditVisibility();
-    animateOverlayMaskVisibility();
-  }
-}
-
-function removeAccount(id) {
-  console.log(id);
-  document.querySelector(`#${id}`).remove();
-  for (let i = 0; i < addedAccounts.length; i++) {
-    if (nameToId(addedAccounts[i].name) === id) {
-      addedAccounts.splice(i, 1);
-      break;
-    }
-  }
-
-  //Remove from local storage.
-  localStorage['account-list'] = JSON.stringify(addedAccounts);
-}
-
-function updateAccount(options = {}) {
-  const { id, name } = options;
-  if (!id || !name) return;
-
-  const liElement = document.querySelector(`#${id}`);
-  liElement.querySelector('.account-title').textContent = name;
-  liElement.id = nameToId(name);
-
-  for (let i = 0; i < addedAccounts.length; i++) {
-    if (nameToId(addedAccounts[i].name) === id) {
-      addedAccounts[i].name = name;
-      break;
-    }
-  }
-  //Update in local storage.
-  localStorage['account-list'] = JSON.stringify(addedAccounts);
 }
 
 // Add the header to allow content to be iframed
